cd501214d4eafc490b6373103a5c1c5f4478d90b
AntoniStachowski <antonistachowski@gmail.com>
2023-06-13 00:39:46 +0200

    Bullshit

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = function (_env, argv) {
+    const isProduction = argv.mode === "production";
+    const isDevelopment = !isProduction;
+
+    return {
+        devtool: isDevelopment && "cheap-module-source-map",
+        entry: "./src/index.js",
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: "assets/js/[name].[contenthash:8].js",
+            publicPath: "/"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            cacheDirectory: true,
+                            cacheCompression: false,
+                            envName: isProduction ? "production" : "development"
+                        }
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+                        "css-loader"
+                    ]
+                },
+                {
+                    test: /\.(png|jpg|gif)$/i,
+                    use: {
+                        loader: "url-loader",
+                        options: {
+                            limit: 8192,
+                            name: "static/media/[name].[hash:8].[ext]"
+                        }
+                    }
+                },
+                {
+                    test: /\.svg$/,
+                    use: ["@svgr/webpack"]
+                }
+            ]
+        },
+        resolve: {
+            extensions: [".js", ".jsx"]
+        },
+        plugins: [
+            isProduction &&
+            new MiniCssExtractPlugin({
+                filename: "assets/css/[name].[contenthash:8].css",
+                chunkFilename: "assets/css/[name].[contenthash:8].chunk.css"
+            }),
+            new webpack.DefinePlugin({
+                'process.env': JSON.stringify(dotenv.parsed),
+                'process.platform': JSON.stringify(process.platform),
+                "process.env.NODE_ENV": JSON.stringify(
+                    isProduction ? "production" : "development"
+                )
+            }),
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, "public/index.html"),
+                inject: true
+            })
+        ].filter(Boolean)
+    };
+};
